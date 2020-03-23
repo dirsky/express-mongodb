18b7593fe1ae4e5972606b3cb3c27eca2cd8d917
xuguozhong <dirsky@126.com>
2020-03-23 11:35:19 +0800

    Initial commit

---
+++ Readme.txt
@@ -0,0 +1,4 @@
+express -e --git express-mongodb
+npm i
+
+npm i mongoose -S
---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+router.post('/reg', function(req, res, next) {
+
+  var b = req.body;
+  var _user={
+    username: b.username,
+    password: b.password
+  }
+
+  var user=new User(_user);
+
+  user.save(function(err,user){
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log('注册成功');
+    res.send('注册成功');
+  })
+});
+
+module.exports = router;
