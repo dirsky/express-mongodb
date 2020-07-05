ad14e5227565d5d92bd33bf106a83fd0b8bd7db4
frank-mac <dirsky@126.com>
2020-07-05 12:10:45 +0800

    理解整体路由的配置方式是叠加的形式

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET users listing.
+* app.use('/users', usersRouter);
+* 结果路由/users加上当前文件的路由
+* */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
